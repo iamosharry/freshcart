fe206f0e3dfcf7a41f98f27536409cdd4232b62f
web3-dar <Onlineagentalmashell@gmail.com>
2025-02-10 22:46:49 +0100

    Fixing database socket issue for remote connection

--- freshcart/settings.py
+++ freshcart/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'freshcart.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME', default='freshcart'),
-        'USER': config('DB_USER', default='postgres'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),  # Ensure this is 5432
+        'NAME': 'freshcart',
+        'USER': 'postgres',
+        'PASSWORD': 'p=///////@Uzumaki1',
+        'HOST': 'localhost',
+        'PORT': '5432',  # Ensure this is 5432
     }
 }
 
