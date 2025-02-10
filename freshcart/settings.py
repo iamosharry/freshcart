12af164726e0c4fd1f4947e2eb6aaa594321db14
web3-dar <Onlineagentalmashell@gmail.com>
2025-02-10 16:42:16 +0100

    Fixed database socket issue for remote connection

--- freshcart/settings.py
+++ freshcart/settings.py
@@ -81,6 +81,7 @@ WSGI_APPLICATION = 'freshcart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -91,7 +92,6 @@ DATABASES = {
         'PORT': config('DB_PORT', default='3306'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'unix_socket': None  # Ensures TCP is used, not the local socket
         },
     }
 }
